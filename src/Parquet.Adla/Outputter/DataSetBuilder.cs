a2466a7bdcc09ac4e5f56377bc9c084f44c8857c
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-07-28 11:47:18 +0100

    kick

--- src/Parquet.Adla/Outputter/DataSetBuilder.cs
+++ src/Parquet.Adla/Outputter/DataSetBuilder.cs
@@ -28,7 +28,7 @@ namespace Parquet.Adla.Outputter
          {
             var se = new SchemaElement(column.Name, column.Type);
          }
-         //kickkk
+         //kickkkk
       }
 
       private Row ToRow(IRow row)
